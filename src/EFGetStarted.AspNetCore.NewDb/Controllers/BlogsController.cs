222e2059327647fd8d7a3527200338706dc2b40d
12122 <jeraytang@1212.com>
2017-01-10 14:55:16 +0800

    添加项目文件。

---
+++ src/EFGetStarted.AspNetCore.NewDb/Controllers/BlogsController.cs
@@ -0,0 +1,78 @@
+﻿using EFGetStarted.AspNetCore.NewDb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFGetStarted.AspNetCore.NewDb.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private BloggingContext _context;
+
+        public BlogsController(BloggingContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Blogs.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Blog blog)
+        {
+            try
+            {
+                List<Blog> list = new List<Blog>();
+                for (int i = 0; i < 5000; i++)
+                {
+                    Blog lg = new Blog();
+                    lg.Url = System.Guid.NewGuid().ToString();
+                    list.Add(lg);
+                }
+               var  dt_st = DateTime.Now.Ticks;
+                _context.AddRange(list);
+           var result = _context.SaveChanges();
+                var dt_end = DateTime.Now.Ticks;
+                var diff = (dt_end - dt_st) * Math.Pow(10,-7);
+                Console.WriteLine(diff);
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+            return View(blog);
+        }
+
+        public IActionResult Modify()
+        {
+            var blg = _context.Blogs.First();
+            blg.Url = DateTime.Now.ToString("yyyy-MM-dd HH:mm:fff");
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Delete()
+        {
+            var blg = _context.Blogs.First();
+            _context.Blogs.Remove(blg);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/EFGetStarted.AspNetCore.NewDb/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using EFGetStarted.AspNetCore.NewDb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFGetStarted.AspNetCore.NewDb
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //var connection = Configuration.GetConnectionString("sqlserverString");
+            //services.AddDbContext<BloggingContext>(options => options.UseSqlServer(connection));
+
+            var connection = Configuration.GetConnectionString("mysqlString");
+            services.AddDbContext<BloggingContext>(option => option.UseMySql(connection));
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
